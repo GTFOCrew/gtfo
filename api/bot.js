484a250de4296af2aa1298a732c1018ab5649dd5
melchor629 <melchor9000@gmail.com>
2019-12-24 17:11:07 +0100

    move bot into its own folder

--- api/bot.js
+++ api/bot.js
@@ -1,17 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-import _ from 'lodash'
-import Telegraf from 'telegraf'
-
-const compliments = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '/_data/compliments.json'))
-)
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start(ctx => ctx.reply('welcome'))
-bot.help(ctx => ctx.reply('send me a sticker'))
-bot.on('sticker', ctx => ctx.reply('👍'))
-bot.hears('yo', ctx => ctx.reply('wassup'))
-bot.hears(/illo/gi, ctx => ctx.reply(_.sample(compliments), { reply_to_message_id: ctx.message.message_id }))
+import bot from './_bot'
 
 export default bot.webhookCallback('/api/bot')
