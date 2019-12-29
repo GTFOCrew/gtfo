1889822a50547beacc2c4931dc73c3c76e6bb687
Neko250 <carlos.aguilar.112358@gmail.com>
2019-12-29 14:52:42 +0100

    fix: debug middleware

--- api/_bot/middlewares/debug.js
+++ api/_bot/middlewares/debug.js
@@ -1,4 +1,10 @@
+import _ from 'lodash'
+
 export default (ctx, next) => {
-  console.debug(`> ${ctx.message.from.username}: ${ctx.message.text}`)
+  const from = _.get(ctx, 'from.username', 'user')
+  const update = _.get(ctx, 'message.text') || _.get(ctx, 'updateSubTypes[0]')
+
+  console.debug(`> ${from}: ${update}`)
+
   return next()
 }
