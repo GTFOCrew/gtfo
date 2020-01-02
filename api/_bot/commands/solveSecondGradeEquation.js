68476a73de14a484c71462598155cce26a812b35
Aleks <alec.augal@gmail.com>
2020-01-02 22:55:53 +0100

    Feature/17 imaginary solutions (#18)
    * Renamed number to sulution since some of them will have an imaginary part
    
    * Now supports imaginary solutions and also special edge cases in the 2nd grade eq
    
    * I put some unrecheable code, but don't worry, fixed

--- api/_bot/commands/solveSecondGradeEquation.js
+++ api/_bot/commands/solveSecondGradeEquation.js
@@ -10,11 +10,22 @@ export default ctx => {
   const a = parseFloat(args[0])
   const b = parseFloat(args[1])
   const c = parseFloat(args[2])
+
+  if (a === 0 && b !== 0) {
+    return ctx.replyMessage(
+      `This is a 1st grade equation ${returnString(b)}x${returnString(
+        c
+      )}=0 and its solution is x=${c / b}`
+    )
+  } else if (a === 0 && b === 0 && c !== 0) {
+    return ctx.replyMessage(`Bro wtf`)
+  } else if (c === 0) {
+    return ctx.replyMessage(`Bro you kidding, x=0 it's trivial...`)
+  }
+
   const sol = computeSolution(a, b, c)
 
-  if (!sol) {
-    return ctx.replyMessage('The equation does not have real solutions')
-  }
+  const alsoImaginarySolution = typeof sol.sol1 === 'string' ? 'imaginary' : ''
 
   const basicMessage = `The solution to \`${returnString(a)}x²${returnString(
     b
@@ -22,32 +33,42 @@ export default ctx => {
 
   if (isDoubleSolution(sol)) {
     return ctx.replyMessage(
-      `${basicMessage} \`x=${sol.sol1}\` (double solution)`
+      `${basicMessage} \`x=${sol.sol1}\` (double ${alsoImaginarySolution} solution)`
     )
   }
 
   return ctx.replyMessage(
-    `${basicMessage} \`x1=${sol.sol1}\` and \`x2=${sol.sol2}\``
+    `${basicMessage} \`x1=${sol.sol1}\` and \`x2=${sol.sol2}\` ${alsoImaginarySolution}`
   )
 }
 
 const computeSolution = (a, b, c) => {
   if (b === 0) {
     const sol = Math.sqrt(c / a)
-    return isNaN(sol) ? null : { sol1: sol, sol2: -sol }
+    return { sol1: sol, sol2: -sol }
   }
   const sqrt = Math.sqrt(b * b - 4 * a * c)
   if (isNaN(sqrt)) {
-    return null
+    return computeImaginarySolution(a, b, c)
   }
   return { sol1: (-b + sqrt) / (2 * a), sol2: (-b - sqrt) / (2 * a) }
 }
 
-const returnString = number => {
-  if (number > 0) {
-    return `+${number}`
+const returnString = solution => {
+  if (solution > 0) {
+    return `+${solution}`
   } else {
-    return number.toString()
+    return `${solution}`
+  }
+}
+
+const computeImaginarySolution = (a, b, c) => {
+  const sqrt = Math.sqrt(Math.abs(b * b - 4 * a * c))
+  const solution = { sol1: -b / (2 * a), sol2: -b / (2 * a) }
+  const imaginarySolution = { sol1: (sqrt / 2) * a, sol2: (-sqrt / 2) * a }
+  return {
+    sol1: `${solution.sol1}${returnString(imaginarySolution.sol1)}i`,
+    sol2: `${solution.sol2}${returnString(imaginarySolution.sol2)}i`
   }
 }
 
