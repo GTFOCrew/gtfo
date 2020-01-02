ded507ace67b900614d8c1fe9ad67639cd8a6d87
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-01-02 21:40:41 +0100

    Fixes #15

--- api/_bot/commands/solveSecondGradeEquation.js
+++ api/_bot/commands/solveSecondGradeEquation.js
@@ -6,32 +6,36 @@ export default ctx => {
         ' solve the 2nd grade equation `ax²+bx+c=0`'
     )
   }
-  
+
   const a = parseFloat(args[0])
   const b = parseFloat(args[1])
   const c = parseFloat(args[2])
   const sol = computeSolution(a, b, c)
-  
+
   if (!sol) {
     return ctx.replyMessage('The equation does not have real solutions')
   }
-  
+
   const basicMessage = `The solution to \`${returnString(a)}x²${returnString(
     b
   )}x${returnString(c)}=0\` equation is`
-  
+
   if (isDoubleSolution(sol)) {
     return ctx.replyMessage(
       `${basicMessage} \`x=${sol.sol1}\` (double solution)`
     )
   }
-  
+
   return ctx.replyMessage(
     `${basicMessage} \`x1=${sol.sol1}\` and \`x2=${sol.sol2}\``
   )
 }
 
 const computeSolution = (a, b, c) => {
+  if (b === 0) {
+    const sol = Math.sqrt(c / a)
+    return isNaN(sol) ? null : { sol1: sol, sol2: -sol }
+  }
   const sqrt = Math.sqrt(b * b - 4 * a * c)
   if (isNaN(sqrt)) {
     return null
