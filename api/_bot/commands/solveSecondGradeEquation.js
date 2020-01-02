0684cf9b7a402e25c622d2edc39c5bbef0063e3c
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-01-02 21:52:31 +0100

    Renamed number to sulution since some of them will have an imaginary part

--- api/_bot/commands/solveSecondGradeEquation.js
+++ api/_bot/commands/solveSecondGradeEquation.js
@@ -6,26 +6,26 @@ export default ctx => {
         ' solve the 2nd grade equation `ax²+bx+c=0`'
     )
   }
-  
+
   const a = parseFloat(args[0])
   const b = parseFloat(args[1])
   const c = parseFloat(args[2])
   const sol = computeSolution(a, b, c)
-  
+
   if (!sol) {
     return ctx.replyMessage('The equation does not have real solutions')
   }
-  
+
   const basicMessage = `The solution to \`${returnString(a)}x²${returnString(
     b
   )}x${returnString(c)}=0\` equation is`
-  
+
   if (isDoubleSolution(sol)) {
     return ctx.replyMessage(
       `${basicMessage} \`x=${sol.sol1}\` (double solution)`
     )
   }
-  
+
   return ctx.replyMessage(
     `${basicMessage} \`x1=${sol.sol1}\` and \`x2=${sol.sol2}\``
   )
@@ -39,12 +39,14 @@ const computeSolution = (a, b, c) => {
   return { sol1: (-b + sqrt) / (2 * a), sol2: (-b - sqrt) / (2 * a) }
 }
 
-const returnString = number => {
-  if (number > 0) {
-    return `+${number}`
+const returnString = solution => {
+  if (solution > 0) {
+    return `+${solution}`
   } else {
-    return number.toString()
+    return `${solution}`
   }
 }
+
+const computeImaginarySolution = () => {}
 
 const isDoubleSolution = ({ sol1, sol2 }) => sol1 === sol2
