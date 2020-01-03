a0f9bb511bcdca9f4f8fbf275795700998058ed8
Neko250 <carlos.aguilar.112358@gmail.com>
2020-01-04 00:37:54 +0100

    feat: add pr command

---
+++ api/_bot/commands/pr.js
@@ -0,0 +1,12 @@
+import fs from 'fs'
+import { data } from '../fn'
+
+export default async ctx =>
+  ctx.replyWithPhoto(
+    {
+      source: await fs.promises.readFile(data('img/pr.jpg'))
+    },
+    {
+      reply_to_message_id: ctx.message.message_id
+    }
+  )
