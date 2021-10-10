06733739f0d2d7f4d5f3ca9f9c500741cf3d42bb
melchor629 <melchor9000@gmail.com>
2021-10-10 17:31:14 +0200

    chore: update telegraph

--- api/_bot/index.js
+++ api/_bot/index.js
@@ -1,31 +1,35 @@
-import Telegraf from 'telegraf'
+import { Telegraf } from 'telegraf'
 import { arkangel, help, math, pr, quote, solve, start } from './commands'
 import { argParser, debug, error, replyMessage } from './middleware'
 import { compliment } from './reactions'
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const createBot = (botToken) => {
+  const bot = new Telegraf(botToken)
 
-// development middleware
-if (process.env.NODE_ENV !== 'production') {
-  bot.use(debug)
+  // development middleware
+  if (process.env.NODE_ENV !== 'production') {
+    bot.use(debug)
+  }
+
+  // middleware
+  bot.catch(error)
+  bot.use(replyMessage)
+  bot.use(argParser)
+
+  // commands
+  bot.start(start)
+  bot.help(help)
+  bot.command('arkangel', arkangel)
+  bot.command('math', math)
+  bot.command('pr', pr)
+  bot.command('quote', quote)
+  bot.command('solve', solve)
+
+  // reactions
+  bot.hears(/\billo\b/gi, compliment)
+  // bot.on('sticker', thumbsUp) // NOTE removed by popular request
+
+  return bot
 }
 
-// middleware
-bot.catch(error)
-bot.use(replyMessage)
-bot.use(argParser)
-
-// commands
-bot.start(start)
-bot.help(help)
-bot.command('arkangel', arkangel)
-bot.command('math', math)
-bot.command('pr', pr)
-bot.command('quote', quote)
-bot.command('solve', solve)
-
-// reactions
-bot.hears(/\billo\b/gi, compliment)
-// bot.on('sticker', thumbsUp) // NOTE removed by popular request
-
-export default bot
+export default createBot
--- api/bot.js
+++ api/bot.js
@@ -1,9 +1,12 @@
-import bot from './_bot'
+import createBot from './_bot'
 import domains from './_bot/domains'
 
 export default (req, res) => {
-  if (req.headers.host !== domains.PRODUCTION)
-    bot.token = process.env.BOT_TOKEN_DEV
+  const bot = createBot(
+    req.headers.host !== domains.PRODUCTION
+      ? process.env.BOT_TOKEN_DEV
+      : process.env.BOT_TOKEN
+  )
 
   bot.webhookCallback('/api/bot')(req, res)
 }
