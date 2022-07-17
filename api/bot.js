cbff0422d33ee0cfae4a898704c7a140f0229c45
melchor629 <melchor9000@gmail.com>
2022-07-17 12:26:02 +0200

    refac: move secrets to env vars

--- api/bot.js
+++ api/bot.js
@@ -2,11 +2,7 @@ import createBot from './_bot'
 import domains from './_bot/domains'
 
 export default (req, res) => {
-  const bot = createBot(
-    req.headers.host !== domains.PRODUCTION
-      ? process.env.BOT_TOKEN_DEV
-      : process.env.BOT_TOKEN
-  )
+  const bot = createBot(process.env.BOT_TOKEN)
 
   bot.webhookCallback('/api/bot')(req, res)
 }
